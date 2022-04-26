c94c89c61d816bfeda9d4476ebbc79c4cd9557f2
Andreas Ink <andreasink17@gmail.com>
2022-04-26 17:05:50 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+pip3 install pyqt6 lxml pillow six ebooklib ply chardet pdfminer.six openpyxl pandas plotly pydub SpeechRecognition
+
+brew install qpdf
+
+cd Downloads/QualCoderInstall-main/QualCoder-master
+
+
+python3 setup.py install
+
+python3 QualCoder-master/qualcoder/__main__.py
